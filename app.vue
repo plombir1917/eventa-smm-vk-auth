ad1eeb1b2bbb33b9484b35be1c779175837d1186
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-04-08 16:24:59 +0300

    feat: add 'vk-auth' into redirect uri

--- app.vue
+++ app.vue
@@ -28,7 +28,7 @@ onMounted(async () => {
 
   VKID.Config.init({
     app: 53404751,
-    redirectUrl: `https://reutov2f.beget.tech`,
+    redirectUrl: `https://reutov2f.beget.tech/vk-auth`,
     scope: 'wall groups photos',
   });
 
